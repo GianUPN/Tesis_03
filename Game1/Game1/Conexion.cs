9679ceae6959c30f7c3b74400b8f29b303e1783a
GianUPN <danycalderonp@hotmail.com>
2015-12-06 18:31:44 -0500

    conexion con BD AZURE

--- Game1/Game1/Conexion.cs
+++ Game1/Game1/Conexion.cs
@@ -12,9 +12,9 @@ namespace Game1
         {
             SqlConnection cn = new SqlConnection();
             cn.ConnectionString =
-                "Data Source=.;Initial Catalog=NumberLand;Integrated Security=True";
+                //"Data Source=.;Initial Catalog=NumberLand;Integrated Security=True";
            // "Data Source=10.144.161.137,1433;Initial Catalog=NumberLand;Integrated Security=False;User ID=xd;Password=G!4ncarlo;Connect Timeout=15;Encrypt=False;Packet Size=4096";
-
+                "Server=tcp:numberlandazure.database.windows.net,1433;Database=NumberLand;User ID=ad@numberlandazure;Password=G!4ncarlo;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             return cn;
         }
     }
